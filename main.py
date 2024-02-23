d6669f66d39481919c61423d2fbb96bc14dd81aa
zvi101 <zvikidzll01@gmail.com>
2024-02-23 10:39:09 +0200

    Creating a function to send a message and display it on the screen

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("hello")
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+
+app = Flask(__name__)
+app.config["SECRET"] = "secretKey"
+socketio = SocketIO(app)
+
+
+@socketio.on('message')
+def printMessage(message):
+    print('Enter message: ' + message)
+    if message != 'user connected':
+        send(message, broadcast=True)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+ 
+if __name__ == "__main__":
+    socketio.run(app, host="localhost")
